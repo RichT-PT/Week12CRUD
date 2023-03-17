c3737e8a3cbdb104edb5ba8b06b52e93aedaaefe
RichT-PT <diverdwn06@outlook.com>
2023-03-17 17:06:39 -0500

    working through jquery errors

--- index.js
+++ index.js
@@ -1,26 +1,68 @@
-class Members {
-  constructor(fname,lname,role,cert){
-    this.fname = fname;
-    this.lname = lname;
+//addEventListener('click', addMember);
+class Member {
+  constructor(firstName,lastName,role,cert){
+    this.firstName = firstName;
+    this.lastName = lastName;
     this.role = role;
     this.cert = cert;
   }
 
 }
+console.log("Beginning of JS code");
+document.getElementById('addMemberClick').addEventListener('click', addMemberForm);
+
+//document.getElementById("submitNewMember").addEventListener("click", MemberService.createMember)
+function addMemberForm() {//!SECTION this section builds the form to add a member
+                          //NOTE - will only be accessible by admin
+  
+  const formDiv = document.getElementById('addMemberForm');
+  const form = 
+    `<form>
+
+      <label for="newMemberTitle">New Member Information</label>
+    <div class="form-group">
+      <input type="name" class="form-control" id="id" placeholder="Unit/handle">
+    </div>
+    <div class="form-group">
+      <input type="name" class="form-control" id="fname" placeholder="First Name">
+    </div>
+    <div class="form-group">
+      <input class="form-control" id="lname" placeholder="Last Name">
+    </div>
+    <div class="form-group">
+      <input class="form-control" id="role" placeholder="Role">
+    </div>
+    <div class="form-group">
+      <input class="form-control" id="cert" placeholder="Highest Certification Level">
+    </div>
+    <button id="submitNewMember" type="button" class="btn btn-success">Submit</button>
+    </form>
+    `;
+  const newForm = document.createElement('form');
+  newForm.innerHTML = form;
+  document.querySelector('body').appendChild(newForm);
+  document.getElementById('addMemberClick').removeEventListener("click", addMemberForm);
+  document.getElementById('submitNewMember').addEventListener('click', MemberService.createMember);
+
+  
+  }  
+
 class MemberService {
-  static url = 'https://app.apidog.com/web/project/359481'
-  static getAllMembers(){
+  
+  static url = 'https://640b8d1365d3a01f981d92a1.mockapi.io'
+
+  static getAllMembers(){//!SECTION will return all the members
     return $.get(this.url);
   }
-  static getMember(id){
+  static getMember(id){//will get an individual member
     return $.get(this.url + `/${id}`);
   }
   static createMember(member) {
     return $.post(this.url, member);
   }
-  static updateMembers(member) {
+  static updateMember(member) {//NOTE - this is the ajax method
     return $.ajax({
-      url:this.url + `/${member._id}`,
+      url:this.url + `/${member.id}`,
       dataType: 'json',
       data: JSON.stringify(member),
       contentType: 'application/json',
@@ -28,78 +70,62 @@ class MemberService {
     });
   }
   static deleteMember(id) {
-    return $.ajac({
+    return $.ajax({
       url: this.url + `/${id}`,
       type: 'DELETE'
     });
-    
   }
 }
-
-
-function addMemberToRoster() {
-    Swal.fire({
-      title: "Add Member",
-      html:
-        '<input id="id" type="hidden">' +
-        '<input id="fname" class="swal2-input" placeholder="First">' +
-        '<input id="lname" class="swal2-input" placeholder="Last">' +
-        '<input id="role" class="swal2-input" placeholder="role">' +
-        '<input id="cert" class="swal2-input" placeholder="Highest Cert:">',
-      focusConfirm: false,
-      preConfirm: () => {
-        userCreate();
-      },
-    });
-  }
   
-  function addMember() {
-    const fname = document.getElementById("fname").value;
-    const lname = document.getElementById("lname").value;
-    const role = document.getElementById("role").value;
-    const cert = document.getElementById("cert").value;
-  
-    const xhttp = new XMLHttpRequest();
-    xhttp.open("POST", "https://www.mecallapi.com/api/users/create");
-    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhttp.send(
-      JSON.stringify({
-        fname: fname,
-        lname: lname,
-        username: role,
-        email: cert,
-        avatar: "https://www.mecallapi.com/users/cat.png",
-      })
-    );
-    xhttp.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
-        const objects = JSON.parse(this.responseText);
-        Swal.fire(objects["message"]);
-        loadTable();
-      }
-    };
-  }
   class DOMManager {
     static members;
     static getAllMembers(){
-      MemberService.getAllMembers(),then(members => this.render(members));
-
+      MemberService.getAllMembers().then(members => this.render(members));
     }
     static render(members){
       this.members = members;
-      $('#app').empty();
+      $('#newMemberBody').empty();
       for (let member of members){
-        $('#app').prepend(
+        $('#newMemberBody').prepend(
           `<tr id="${member.id}">
-            <th scope="col">${members[i]+1}</th>
-            <td scope="col"><intput type="text" id = "${member._id}-fname" scope="col" placeholder="fname"</td>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Role</th>
-            <th scope="col">Highest Cert:</th>
+            <td><button type="button" class="btn btn-danger pull-left" style: float>Delete</button></td>
+            <td scope="col">${[i]+1}</td>
+            <td scope="col"></td>
+            <td scope="col"></td>
+            <td scope="col"></td>
+            <td scope="col"></td>
+            <td scope="col"></td>
           `
         )
       }
+    }
+    //let newMember = [];
+    
+    static addMember(firstName, lastName, role, cert){
+      this.firstName = document.getElementById("fname").value;
+      this.lastName = document.getElementById("lname").value;
+      this.role = document.getElementById("role").value;
+      this.cert = document.getElementById("cert").value;
+      MemberService.createMember(new Member(firstName, lastName, role, cert))
+      .then(() => {
+        //return MemberService.getAllMembers();
+        return $.ajax({
+          url:this.url,
+          dataType: 'json',
+          data: JSON.stringify(member),
+          contentType: 'application/json',
+          type: 'POST'
+        });
+      })
+      .then((members) => this.render(members));
+    }
+    static deleteMember(id){
+      MemberService.deleteMember(id)
+      .then(() => {
+        return MemberService.getAllMembers();
+      })
+      .then((members) => this.render(members));
+
     }
   }
   DOMManager.getAllMembers();
